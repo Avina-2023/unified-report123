4517636a7b0444097f04aeee5f0d43d4cc194600
mmanju83 <mmanju83@gmail.com>
2021-07-01 19:26:51 +0530

    AssessmentReport UI design
    Assessment UI Design

---
+++ src/app/app.module.ts
@@ -0,0 +1,41 @@
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material/material.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { ProfileInfoComponent } from './pages/profile-info/profile-info.component';
+import { DocInfoComponent } from './pages/doc-info/doc-info.component';
+import { AssessmentInfoComponent } from './pages/assessment-info/assessment-info.component';
+import { CompetencyAreasComponent } from './pages/competency-areas/competency-areas.component';
+import { QualityAreaComponent } from './pages/quality-area/quality-area.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProfileInfoComponent,
+    DocInfoComponent,
+    AssessmentInfoComponent,
+    CompetencyAreasComponent,
+    QualityAreaComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    CommonModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA]
+})
+export class AppModule { }
---
+++ src/app/pages/competency-areas/competency-areas.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-competency-areas',
+  templateUrl: './competency-areas.component.html',
+  styleUrls: ['./competency-areas.component.scss']
+})
+export class CompetencyAreasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
