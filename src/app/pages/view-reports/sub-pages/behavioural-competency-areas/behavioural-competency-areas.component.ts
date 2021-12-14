7c6b5e08b55c23552247f6f22a5d7bbe4dd85a99
B-ANKIT <b-ankit@lntecc.com>
2021-12-14 18:03:06 +0530

    Commit with color changes

--- src/app/pages/view-reports/sub-pages/behavioural-competency-areas/behavioural-competency-areas.component.ts
+++ src/app/pages/view-reports/sub-pages/behavioural-competency-areas/behavioural-competency-areas.component.ts
@@ -5,9 +5,8 @@ import { slide } from 'src/app/animations';
   selector: 'app-behavioural-competency-areas',
   templateUrl: './behavioural-competency-areas.component.html',
   styleUrls: ['./behavioural-competency-areas.component.scss'],
-  animations: slide
+  animations: slide,
 })
-
 export class BehaviouralCompetencyAreasComponent implements OnInit {
   @Input() getAllReportsData;
   competancyData = [];
@@ -20,45 +19,68 @@ export class BehaviouralCompetencyAreasComponent implements OnInit {
   competenciesName: any;
   unSortedVerticalData: any;
   unSortedHorizontalData: any;
-  domain = ['#8ac1ed', '#a4dea5', '#f7d096', '#e89694', '#8ac1ed', '#a4dea5', '#f7d096', '#e89694', '#8ac1ed', '#a4dea5', '#f7d096', '#e89694', '#8ac1ed', '#a4dea5', '#f7d096', '#e89694'];
-  verticaldomain = ["#FF8C00", "#0085B6" , "#9DBC5B" , "#28B59A", "#03B8CB", "#FF8C00", "#0085B6" , "#9DBC5B" , "#28B59A", "#03B8CB"];
+  domain = [
+    '#FF8C00',
+    '#0085B6',
+    '#9DBC5B',
+    '#28B59A',
+    '#03B8CB',
+    '#FF8C00',
+    '#0085B6',
+    '#9DBC5B',
+    '#28B59A',
+    '#03B8CB',
+  ];
+  verticaldomain = [
+    '#FF8C00',
+    '#0085B6',
+    '#9DBC5B',
+    '#28B59A',
+    '#03B8CB',
+    '#FF8C00',
+    '#0085B6',
+    '#9DBC5B',
+    '#28B59A',
+    '#03B8CB',
+  ];
   selectedHorizontalChartIndex = '0';
   verticalChartData: any[];
   hideControls = true;
-  constructor() {
-  }
+  constructor() {}
 
   ngOnInit(): void {
-  this.getCompetancyData();
+    this.getCompetancyData();
   }
 
   ngOnChanges() {
     this.getCompetancyData();
   }
 
-
-  onSelect(event) {
-  }
+  onSelect(event) {}
 
   getParticularCompetencySkills(e) {
     this.getHorizontalDataByCompetencyId(e);
   }
 
   getHorizontalDataByCompetencyId(id) {
-    const selectedCompetency = this.competancyData.find((data: any)=> {
+    const selectedCompetency = this.competancyData.find((data: any) => {
       if (data && data.competencyname == id) {
+        this.setColorCodesBasedOnLabel(data.competencyname)
         return data;
-      };
+      }
     });
     if (selectedCompetency) {
-      const filterIndex = this.competancyData.findIndex(data => (data.competencyname == selectedCompetency.competencyname && data.score == selectedCompetency.score));
+      const filterIndex = this.competancyData.findIndex(
+        (data) =>
+          data.competencyname == selectedCompetency.competencyname &&
+          data.score == selectedCompetency.score
+      );
       this.counter = filterIndex != -1 ? filterIndex : this.counter;
     }
   }
 
-
   selectedHorizontalArrayIndex(event, i) {
-    let skill = this.competancyData[i].skills.find((data: any)=> {
+    let skill = this.competancyData[i].skills.find((data: any) => {
       if (data.skillname == event.name && data.score == event.value) {
         return data;
       }
@@ -71,7 +93,7 @@ export class BehaviouralCompetencyAreasComponent implements OnInit {
     this.competancyData[i].areaSkills = area;
   }
 
-  getCompetancyData(){
+  getCompetancyData() {
     this.competancyData = this.getAllReportsData?.behavioralCompetencyDetails;
     if (this.competancyData && this.competancyData.length > 0) {
       // this.competancyData.forEach(element => {
@@ -84,27 +106,27 @@ export class BehaviouralCompetencyAreasComponent implements OnInit {
   }
 
   convertToPercentage() {
-    this.competancyData.forEach(element => {
+    this.competancyData.forEach((element) => {
       if (element.score && element.maxscore) {
         element.actualScore = element.score;
         element.score = this.conversionFormula(100, 100);
       }
-      element.skills.forEach(skills => {
+      element.skills.forEach((skills) => {
         // if (skills.stenScore) {
-          skills.score = skills.stenScore ? skills.stenScore : 0;
+        skills.score = skills.stenScore ? skills.stenScore : 0;
         // }
-      skills.area.forEach(area => {
-        if (area.score && area.maxscore) {
-          area.actualScore = area.score;
-          area.score = this.conversionFormula(area.score, area.maxscore);
-        }
-      });
+        skills.area.forEach((area) => {
+          if (area.score && area.maxscore) {
+            area.actualScore = area.score;
+            area.score = this.conversionFormula(area.score, area.maxscore);
+          }
+        });
       });
     });
   }
 
   conversionFormula(score: number, maxscore: number) {
-    let percentage: number = Number(score) / Number(maxscore) * 100;
+    let percentage: number = (Number(score) / Number(maxscore)) * 100;
     percentage = Number(percentage.toFixed(2));
     return Number.isInteger(percentage) ? percentage : percentage.toFixed(2);
   }
@@ -118,57 +140,80 @@ export class BehaviouralCompetencyAreasComponent implements OnInit {
     });
     this.list = listCount;
     this.verticalChartData.forEach((element, i) => {
-      if(element && element.score) {
-        element.areaColor = this.verticaldomain[i];
+      if (element && element.score) {
+        // element.areaColor = this.verticaldomain[i];
+        element.areaColor =    this.setColorCodesBasedOnLabel(element.competencyname);
       }
     });
   }
   getAreasDataInitialize(i) {
-    this.competancyData.forEach(skills => {
+    this.competancyData.forEach((skills,j) => {
+      console.log(skills,'element')
       if (skills) {
         let areaSingle = [];
         skills.skills.forEach((area, i) => {
+
           if (area) {
-          area.areaColor = this.domain[i];
-          area.area.forEach(element => {
-            element.areaColor = this.domain[i];
-            areaSingle.push(element);
-          });
-        }
+            // area.areaColor = this.domain[i];
+            area.area.forEach((element) => {
+            
+              // element.areaColor = this.domain[i];
+              areaSingle.push(element);
+            });
+          }
         });
         skills.areaSkills = areaSingle;
       }
     });
-}
+  }
 
-resetAreas(i, competency) {
-  let areaSingle = [];
-  this.competancyData[i].skills.forEach((area, i) => {
-    if (area) {
-    area.areaColor = this.domain[i];
-    area.area.forEach(element => {
-      element.areaColor = this.domain[i];
-      areaSingle.push(element);
+  resetAreas(i, competency) {
+    let areaSingle = [];
+    this.competancyData[i].skills.forEach((area, i) => {
+      if (area) {
+        // area.areaColor = this.domain[i];
+        area.area.forEach((element) => {
+          // element.areaColor = this.domain[i];
+          areaSingle.push(element);
+        });
+      }
     });
+    this.competancyData[i].areaSkills = areaSingle;
   }
-  });
-  this.competancyData[i].areaSkills = areaSingle;
-}
 
-onNext() {
-  if (this.counter != this.list.length - 1) {
-    this.counter++;
+  onNext() {
+    if (this.counter != this.list.length - 1) {
+      this.counter++;
+    }
   }
-}
 
-onPrevious() {
-  if (this.counter > 0) {
-    this.counter--;
+  onPrevious() {
+    if (this.counter > 0) {
+      this.counter--;
+    }
   }
-}
 
-dotChange(i) {
-  this.counter = i;
-}
+  dotChange(i) {
+    this.counter = i;
+  }
 
+  setColorCodesBasedOnLabel(labelName: any) {
+    if(labelName == 'Thought Factor'){
+        this.domain = ['#8EC031'];
+        return '#8EC031';
+    } if(labelName == 'Emotion Factor'){
+      this.domain = ['#FFBB48'];
+        return '#FFBB48'
+    } if(labelName == 'Core/Personal Factor'){
+      this.domain = ['#5460A7'];
+      return '#5460A7'
+      }if(labelName == 'Interpersonal Factor'){
+        this.domain = ['#F08145'];
+        return '#F08145'
+    }
+    else {
+      this.domain = ['#03B8CB'];
+        return '#03B8CB'
+    }
+  }
 }
