ecdb67a12a8d7ee87d3425480a99db49ba4352c4
Karthikeyan <karthikeyanb@lntecc.com>
2022-02-25 17:59:04 +0530

    Behavioural report UI for PDF Download implemented
    Behavioural report UI for PDF Download implemented

---
+++ src/app/pages/behavioural-report/behavioural-pdf-report-download/behavioural-pdf-report-download.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-behavioural-pdf-report-download",
+  templateUrl: "./behavioural-pdf-report-download.component.html",
+  styleUrls: ["./behavioural-pdf-report-download.component.scss"]
+})
+
+export class BehaviouralPdfReportDownloadComponent implements OnInit {
+  
+  constructor() { 
+
+  }
+
+  ngOnInit() {
+
+  }
+}
