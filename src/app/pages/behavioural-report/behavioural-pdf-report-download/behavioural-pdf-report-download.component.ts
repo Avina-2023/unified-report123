2192308ca43bd94d01de21b0d403866e07d1db5e
B-ANKIT <b-ankit@lntecc.com>
2022-03-02 11:45:20 +0530

    Commit with BEHAVIOURAL PERSONAL PROFILE pdf data binding

--- src/app/pages/behavioural-report/behavioural-pdf-report-download/behavioural-pdf-report-download.component.ts
+++ src/app/pages/behavioural-report/behavioural-pdf-report-download/behavioural-pdf-report-download.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, Input, OnInit } from "@angular/core";
+import * as moment from "moment";
+import { AppConfigService } from "src/app/utils/app-config.service";
 
 @Component({
   selector: "app-behavioural-pdf-report-download",
@@ -7,12 +9,41 @@ import { Component, OnInit } from "@angular/core";
 })
 
 export class BehaviouralPdfReportDownloadComponent implements OnInit {
-  
-  constructor() { 
+  isaccess:any;
+  @Input() data;
+  highestEducation: any;
+  getAllBasicData: any;
+  getAllBehaviourData: any;
+  getAllBehaviourAPIDetails: any;
+  constructor(private appconfig: AppConfigService,) { 
 
   }
 
   ngOnInit() {
+    this.isaccess = this.appconfig.isComingFromMicroCert();
+    if(this.data){
+      console.log(this.data,'asdadasd')
+      this.getReportData();
+    }
+  }
 
+  getReportData(){
+    this.getAllBehaviourData = this.data.data ? this.data.data : null;
+          this.getAllBehaviourAPIDetails = this.data ? this.data : null;
+          console.log( this.getAllBehaviourAPIDetails,this.data.data )
+          this.getAllBasicData = this.data.basicDetails ? this.data.basicDetails : null;
+          console.log(this.data.data.basicDetails ,'this.getAllBasicData')
+          this.highestEducation = this.getAllBasicData && this.getAllBasicData.education ? this.getAllBasicData.education : [];
+          if (this.highestEducation.length > 0) {
+            let i = this.highestEducation.length - 1;
+            this.highestEducation = this.highestEducation[i];
+          }
+  }
+
+  momentForm(date) {
+    if (date) {
+      const split = moment(date).format('DD/MM/YYYY');
+      return split;
+    }
   }
 }
