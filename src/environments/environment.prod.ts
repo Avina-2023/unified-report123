d433e92f8a76f54136b9974a86fe65f706a0d79e
B-ANKIT <b-ankit@lntecc.com>
2022-05-07 15:08:59 +0530

    Commit with env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -12,5 +12,6 @@ export const environment = {
   NODE_EDGE_URL: 'https://uapedgeservice.lntiggnite.com',
   NODE_URL: 'https://uapcoreservices.lntiggnite.com',
   PROCTOR_URL : 'http://lntproctordev.lntedutech.com/api/storage/',
-  PROCTOR : 'https://lntproctordev.lntedutech.com'
+  PROCTOR : 'https://lntproctordev.lntedutech.com',
+  MONGOCHARTURL: 'https://charts.mongodb.com/charts-microcertuat-gzomw'
 };
