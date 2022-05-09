8dcef41f0bd127058950f745e256503eb57ab3a2
Karthikeyan <karthikeyan.b1@lntedutech.com>
2022-05-09 17:32:19 +0530

    candidate details page UI revamp

--- src/app/pages/view-reports/sub-pages/candidate-profile/candidate-profile.component.ts
+++ src/app/pages/view-reports/sub-pages/candidate-profile/candidate-profile.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Input, TemplateRef, ViewChild } from "@angular/core";
 import { MatDialog } from '@angular/material/dialog';
 import { environment } from 'src/environments/environment';
+import { ActivatedRoute } from '@angular/router';
+import { ApiService } from '../../../../services/api.service';
 
 @Component({
   selector: "app-candidate-profile",
@@ -16,13 +18,22 @@ export class CandidateProfileComponent implements OnInit {
   blobkey = environment.blobKey;
   profilePic: any;
   personalInfo: any;
+  selectedMail: any;
+  totalCount: any;
+  driveUserdata = [];
+  userCount = 0;
 
-  constructor(private dialog: MatDialog) { 
+  constructor(
+    private dialog: MatDialog,
+    private route: ActivatedRoute,
+    private ApiService: ApiService
+    ) { 
 
   }
 
   ngOnInit() {
     this.getDocInfo();
+    this.getRoute();
   }
 
   ngOnChanges() {
@@ -63,5 +74,26 @@ export class CandidateProfileComponent implements OnInit {
     this.personalInfo.mobile = this.getAllReportsData?.mobile;
     this.personalInfo.email = this.getAllReportsData?.email;
     this.personalInfo.qrCodeURL = this.getAllReportsData?.qrCodeURL;
+  }
+
+  getRoute() {
+    this.route.paramMap.subscribe((param: any) => {
+      if (param && param.params && param.params.id) {
+        this.selectedMail = param.params.id;
+          this.getDriveUser();
+
+      }
+    });
+  }
+  getDriveUser() {
+    const FilterData = localStorage.getItem('FilterData');
+    let requestFilterData = JSON.parse(FilterData);
+    this.ApiService.getcandidateList(requestFilterData).subscribe((data: any) => {
+      if(data.success){
+        this.totalCount = data.total_count;
+        this.driveUserdata = data.data;
+        this.userCount = this.driveUserdata.findIndex((data:any) => data.email == this.selectedMail);
+      }
+    });
   }
 }
