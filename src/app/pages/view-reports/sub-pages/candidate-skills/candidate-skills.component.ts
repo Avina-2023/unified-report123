e7ffe163e5c271a44f73ceef251465ab37495348
B-ANKIT <b-ankit@lntecc.com>
2022-05-10 16:26:53 +0530

    Commit with bar chart border radius added

--- src/app/pages/view-reports/sub-pages/candidate-skills/candidate-skills.component.ts
+++ src/app/pages/view-reports/sub-pages/candidate-skills/candidate-skills.component.ts
@@ -1,6 +1,6 @@
 
 import { Component, OnInit, Input } from '@angular/core';
-import { ChartDataSets, ChartOptions, ChartType } from 'chart.js';
+import { ChartDataSets, ChartOptions, ChartType,Chart } from 'chart.js';
 import { ActivatedRoute } from '@angular/router';
 import { ApiService } from 'src/app/services/api.service';
 import { AppConfigService } from 'src/app/utils/app-config.service';
@@ -16,7 +16,7 @@ export class CandidateSkillsComponent implements OnInit {
   candidateSkills:any = [];
   public barChartType: ChartType = 'horizontalBar';
   public barChartLegend = false;
-  public barChartOptions: ChartOptions = {
+  public barChartOptions: any = {
     responsive: true,
     layout: {
       padding: {
@@ -55,15 +55,16 @@ export class CandidateSkillsComponent implements OnInit {
         anchor: 'end',
         align: 'end',
         font: {
-          size: 10,
-        }
+          size: 14,
+        },
+        
       }
     }
 
 
   };
-  public barChartData: ChartDataSets[] = [
-    { data: [], backgroundColor: [],hoverBackgroundColor:[],},
+  public barChartData:any = [
+    { data: [], backgroundColor: [],hoverBackgroundColor:[],borderRadius:15},
 
   ];
   public barChartLabels: string[] = [];
@@ -77,8 +78,8 @@ export class CandidateSkillsComponent implements OnInit {
   isJobFit: any;
   isaccess: any;
   top7Jobs: any = [];
-  constructor( private route: ActivatedRoute,private apiService: ApiService,private appConfig: AppConfigService,) {
-
+  constructor(private route: ActivatedRoute,private apiService: ApiService,private appConfig: AppConfigService,) {
+    this.barChartBorder();
    }
 
   ngOnInit(): void {
@@ -163,11 +164,124 @@ export class CandidateSkillsComponent implements OnInit {
             data: this.barChartValue,
             backgroundColor:this.barChartColorCode,
             hoverBackgroundColor:this.barChartColorCode,
-            barThickness: 50,
+            barThickness: 40,
           }
         ];
       }
      });
+   }
+
+
+   barChartBorder(){
+    Chart['elements'].Rectangle.prototype.draw = function () {
+      var ctx = this._chart.ctx;
+      var vm = this._view;
+      var left, right, top, bottom, signX, signY, borderSkipped, radius;
+      var borderWidth = vm.borderWidth;
+      
+      // Set Radius Here
+      // If radius is large enough to cause drawing errors a max radius is imposed
+      var cornerRadius = 20;
+
+        left = vm.base;
+        right = vm.x;
+        top = vm.y - vm.height / 2;
+        bottom = vm.y + vm.height / 2;
+        signX = right > left ? 1 : -1;
+        signY = 1;
+        borderSkipped = vm.borderSkipped || 'left';
+
+      // Canvas doesn't allow us to stroke inside the width so we can
+      // adjust the sizes to fit if we're setting a stroke on the line
+      if (borderWidth) {
+        // borderWidth shold be less than bar width and bar height.
+        var barSize = Math.min(Math.abs(left - right), Math.abs(top - bottom));
+        borderWidth = borderWidth > barSize ? barSize : borderWidth;
+        var halfStroke = borderWidth / 2;
+        // Adjust borderWidth when bar top position is near vm.base(zero).
+        var borderLeft = left + (borderSkipped !== 'left' ? halfStroke * signX : 0);
+        var borderRight = right + (borderSkipped !== 'right' ? -halfStroke * signX : 0);
+        var borderTop = top + (borderSkipped !== 'top' ? halfStroke * signY : 0);
+        var borderBottom = bottom + (borderSkipped !== 'bottom' ? -halfStroke * signY : 0);
+        // not become a vertical line?
+        if (borderLeft !== borderRight) {
+          top = borderTop;
+          bottom = borderBottom;
+        }
+        // not become a horizontal line?
+        if (borderTop !== borderBottom) {
+          left = borderLeft;
+          right = borderRight;
+        }
+      }
+
+      ctx.beginPath();
+      ctx.fillStyle = vm.backgroundColor;
+      ctx.strokeStyle = vm.borderColor;
+      ctx.lineWidth = borderWidth;
+
+      // Corner points, from bottom-left to bottom-right clockwise
+      // | 1 2 |
+      // | 0 3 |
+      var corners = [
+        [left, bottom],
+        [left, top],
+        [right, top],
+        [right, bottom]
+      ];
+
+      // Find first (starting) corner with fallback to 'bottom'
+      var borders = ['bottom', 'left', 'top', 'right'];
+      var startCorner = borders.indexOf(borderSkipped, 0);
+      if (startCorner === -1) {
+        startCorner = 0;
+      }
+
+      function cornerAt(index) {
+        return corners[(startCorner + index) % 4];
+      }
+
+      // Draw rectangle from 'startCorner'
+      var corner = cornerAt(0);
+      var width, height, x, y, nextCorner, nextCornerId
+      var x_tl, x_tr, y_tl, y_tr;
+      var x_bl, x_br, y_bl, y_br;
+      ctx.moveTo(corner[0], corner[1]);
+
+      for (var i = 1; i < 4; i++) {
+        corner = cornerAt(i);
+        nextCornerId = i + 1;
+        if (nextCornerId == 4) {
+          nextCornerId = 0
+        }
+
+        nextCorner = cornerAt(nextCornerId);
+
+        width = corners[2][0] - corners[1][0];
+        height = corners[0][1] - corners[1][1];
+        x = corners[1][0];
+        y = corners[1][1];
+        radius = cornerRadius;
+
+        // Fix radius being too large        
+        if (radius > Math.abs(height) / 2) {
+          radius = Math.floor(Math.abs(height) / 2);
+        }
+        if (radius > Math.abs(width) / 2) {
+          radius = Math.floor(Math.abs(width) / 2);
+        }
+        ctx.moveTo(x, y);
+        ctx.lineTo(x + width - radius, y);
+        ctx.quadraticCurveTo(x + width, y, x + width, y + radius);
+        ctx.lineTo(x + width, y + height - radius);
+        ctx.quadraticCurveTo(x + width, y + height, x + width - radius, y + height);
+        ctx.lineTo(x, y + height);
+      }
+      ctx.fill();
+      if (borderWidth) {
+        ctx.stroke();
+      }
+    };
    }
 
   //  getPersonalInfo() {
