9413680d11a2dac9e5e64aca4bf3f9c6e8bc3ccd
B-ANKIT <b-ankit@lntecc.com>
2022-05-24 10:02:56 +0530

    Commit with candidate assessment report design

--- src/app/pages/candidate-assessment-report/sub-pages/complexity-analysis/complexity-analysis.component.ts
+++ src/app/pages/candidate-assessment-report/sub-pages/complexity-analysis/complexity-analysis.component.ts
@@ -6,7 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./complexity-analysis.component.scss']
 })
 export class ComplexityAnalysisComponent implements OnInit {
+  public barChartOptions: any = {
+    scaleShowVerticalLines: false,
+    responsive: true,
+    scales: {
+            xAxes: [{
+                stacked: true
+            }],
+            yAxes: [{
+                stacked: true
+            }]
+        }
+  };
 
+
+  public barChartLabels: string[];
+  public barChartType: string = 'horizontalBar';
+  public barChartLegend: boolean = false;
+  
+
+  public barChartData: any[] = [
+    { data: [], label: 'Volume Sales' },
+    { data: [], label: 'Value Sales' }
+  ];
   constructor() { }
 
   ngOnInit(): void {
