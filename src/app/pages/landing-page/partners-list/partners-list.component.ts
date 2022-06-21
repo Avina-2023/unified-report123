3654564313e7b5cd35565d31be0fc5d310f983d6
B-ANKIT <b-ankit@lntecc.com>
2022-06-21 16:33:02 +0530

    Commit with skill exchange new landing page

--- src/app/pages/landing-page/partners-list/partners-list.component.ts
+++ src/app/pages/landing-page/partners-list/partners-list.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { DragScrollComponent } from 'ngx-drag-scroll';
+import { OwlOptions } from 'ngx-owl-carousel-o';
 
 @Component({
   selector: 'app-partners-list',
@@ -6,10 +8,70 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./partners-list.component.scss']
 })
 export class PartnersListComponent implements OnInit {
-
+  @ViewChild(DragScrollComponent) ds: DragScrollComponent;
+  // leftNavDisabled = false;
+  // rightNavDisabled = false;
+  customOptions: OwlOptions = {
+    loop: true,
+    mouseDrag: false,
+    touchDrag: false,
+    pullDrag: false,
+    autoplay: true,
+    animateIn: 'fadeIn',
+    animateOut: 'fadeOut',
+    autoplayTimeout: 1000,
+    autoplayHoverPause: false,
+    dots: false,
+    navSpeed: 1000,
+    navText: ['', ''],
+    nav: false,
+    autoHeight: true,
+    autoWidth: true,
+    responsive: {
+      0: {
+        items: 3,
+      },
+      200: {
+        items: 3,
+      },
+      400: {
+        items: 3,
+      },
+    
+    }
+  };
+  footerImag = [
+    {
+      image: 'assets/images/landing/L&T.png'
+    },
+    {
+      image: 'assets/images/landing/L&T_NXT.png'
+    },
+    {
+      image: 'assets/images/landing/L&T_Services_Stacked.png'
+    },
+    {
+      image: 'assets/images/landing/LTI.png'
+    }
+  ]
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  
+  // moveLeft() {
+  //   this.ds.moveLeft();
+  // }
+  // moveRight() {
+  //   this.ds.moveRight();
+  // }
+  // leftBoundStat(reachesLeftBound: boolean) {
+  //   this.leftNavDisabled = reachesLeftBound;
+  // }
+
+  // rightBoundStat(reachesRightBound: boolean) {
+  //   this.rightNavDisabled = reachesRightBound;
+  // }
 
 }
