5bab9ee796f605177fb3a20b8933ca07a6075da6
B-ANKIT <b-ankit@lntecc.com>
2022-07-01 14:56:25 +0530

    Commit with banner

--- src/app/pages/landing-page/landing-page.component.ts
+++ src/app/pages/landing-page/landing-page.component.ts
@@ -10,8 +10,10 @@ import { environment } from 'src/environments/environment.prod';
 export class LandingPageComponent implements OnInit {
   @ViewChild('register', {static: false}) register: TemplateRef<any>;
   @ViewChild('jobseekers', {static: false}) jobseekers: TemplateRef<any>;
+  @ViewChild('skillexchange', {static: false}) skillexchange: TemplateRef<any>;
   baseUrl= environment.OFFCAMPUSDRIVE
   sectiondialogRef: any;
+  skillBanner:any;
   owlCarouselOptions: OwlOptions = {
     // loop: true,
     mouseDrag: false,
@@ -38,6 +40,10 @@ export class LandingPageComponent implements OnInit {
   constructor(private matDialog: MatDialog) { }
 
   ngOnInit(): void {
+    setTimeout(() => {
+      this.skillExchangeBanner();
+    }, 100);
+  
   }
 
   
@@ -69,6 +75,15 @@ export class LandingPageComponent implements OnInit {
       height: '524px',
       panelClass: 'loginpopover',
       
+    });
+  }
+
+
+  skillExchangeBanner(){
+    this.skillBanner = this.matDialog.open(this.skillexchange, {
+      width: '100%',
+      height: '524px',
+      panelClass: 'loginpopover',
     });
   }
 
