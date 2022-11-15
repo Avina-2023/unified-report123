03b5a501a2346c0c665dcf0e77b0aecd646f81f6
venkat <venkat.j@lntecc.com>
2022-11-15 16:15:38 +0530

    Find & Apply Jobs
    UI Change

--- src/app/pages/candidate/candidate.module.ts
+++ src/app/pages/candidate/candidate.module.ts
@@ -1,13 +1,16 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CandidateComponent } from './candidate.component';
 import { CandidateRoutes } from './candidate.routing';
+import { MaterialModule } from 'src/app/material/material.module';
 
 @NgModule({
   imports: [
     CommonModule,
-    CandidateRoutes
+    CandidateRoutes,
+    MaterialModule
   ],
-  declarations: [CandidateComponent]
+  declarations: [CandidateComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA]
 })
 export class CandidateModule { }
