6e9c01ca04a06bc913dfd1b504cb6701ef7e7f25
Gokul Appaswamy <77961879+GokulALT@users.noreply.github.com>
2022-11-17 17:58:59 +0530

    added dashboard component

--- src/app/pages/candidate/job-dashboard/job-dashboard.component.ts
+++ src/app/pages/candidate/job-dashboard/job-dashboard.component.ts
@@ -7,6 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class JobDashboardComponent implements OnInit {
 
+  dashboardCards: any = [
+    {
+      'name': 'jobs Available',
+      'count': "60"
+    },
+    {
+      'name': 'jobs Applied',
+      'count': "62"
+    },
+    {
+      'name': 'Profile Viewed',
+      'count': "23"
+    },
+    {
+      'name': 'Shortlisted',
+      'count': "10"
+    }
+  ]
   constructor() { }
 
   ngOnInit() {
