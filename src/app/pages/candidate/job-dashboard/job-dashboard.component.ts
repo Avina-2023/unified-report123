a3f3523a3ada5d5fb82f716f739ca39b4eaf65ed
murali <murali.b@lntecc.com>
2022-11-23 09:49:15 +0530

    candidate dashboard chart

--- src/app/pages/candidate/job-dashboard/job-dashboard.component.ts
+++ src/app/pages/candidate/job-dashboard/job-dashboard.component.ts
@@ -1,5 +1,5 @@
 import { ThrowStmt } from '@angular/compiler';
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, SimpleChange, ViewChild } from '@angular/core';
 import {
   ApexAxisChartSeries,
   ApexChart,
@@ -44,29 +44,18 @@ export class JobDashboardComponent implements OnInit {
   public jobApplied: any;
   public profileView: any;
   public shortlisted: any;
-  public ChartjobAvailableCount: any;
-  public allyears: any = { year: ['2022', '2021', '2020'] }
+  public ChartData: any = [];
+  public objDetails: any;
+
+  public allyears = [
+    { value: '0', year: '2022' },
+    { value: '1', year: '2021' },
+    { value: '2', year: '2020' },
+  ];
 
   constructor(private apiService: ApiService, private appConfig: AppConfigService) {
     this.chartOptions = {
-      series: [
-        {
-          name: "Jobs Available",
-          data: [44, 55, 1, 57, 56, 61, 58, 63, 60, 98, 1, 66]
-        },
-        {
-          name: "Jobs Applied",
-          data: [76, 85, 101, 1, 4, 98, 87, 98, 105, 91, 114, 94]
-        },
-        {
-          name: "Profile Viewed",
-          data: [35, 41, 36, 34, 53, 1, 26, 45, 48, 52, 53, 41]
-        },
-        {
-          name: "Shortlisted",
-          data: [1, 6, 6, 5, 2, 3, 6, 88, 8, 2, 3, 1]
-        }
-      ],
+      series: [],
       chart: {
         type: "bar",
         height: 350,
@@ -108,52 +97,78 @@ export class JobDashboardComponent implements OnInit {
           "Dec"
         ]
       },
-      // yaxis: {
-      //   title: {
-      //     text: "$ (thousands)"
-      //   }
-      // },
       fill: {
         opacity: 1,
         colors: ['#26BBEF', '#FF9A78', '#10E596', '#FDBC64']
       },
-      // tooltip: {
-      //   y: {
-      //     formatter: function(val) {
-      //       return "$ " + val + " thousands";
-      //     }
-      //   }
-      // }
     };
   }
   ngOnInit(): void {
-    console.log(this.allyears)
     this.getCandidateDashBoard()
   }
+
+
+  // Based on selct year dashboard data 
+
+  selectYear(e) {
+    if (e.value == 0) {
+      this.getCandidateDashBoard()
+    }
+    else if (e.value == 1) {
+      this.year = this.appConfig.getCurrentYear() - 1
+      this.email = 'deenabandhutekarla@gmail.com'
+      // this.email = localStorage.getItem('email')
+      const obj1 = {};
+      if (Object.keys(obj1).length === 0) {
+        Object.assign(obj1, { "year": this.year, "email": this.email });
+      }
+      this.apiService.candidateDashboard(obj1).subscribe((res: any) => {
+        if (res.success) {
+          this.ChartData = res.data.series;
+          this.chartOptions.series.push(this.ChartData)
+          console.log(this.ChartData)
+        }
+      })
+    }
+    else if (e.value == 2) {
+      this.year = this.appConfig.getCurrentYear() - 2
+      this.email = 'deenabandhutekarla@gmail.com'
+      // this.email = localStorage.getItem('email')
+      const obj1 = {};
+      if (Object.keys(obj1).length === 0) {
+        Object.assign(obj1, { "year": this.year, "email": this.email });
+      }
+      this.apiService.candidateDashboard(obj1).subscribe((res: any) => {
+        if (res.success) {
+          this.ChartData = res.data.series;
+          this.chartOptions.series.push(this.ChartData)
+          console.log(this.ChartData)
+        }
+      })
+    }
+  }
+
+
+// candidate Dashboard Barchart   
+
 
   getCandidateDashBoard() {
     this.year = this.appConfig.getCurrentYear()
     this.email = 'deenabandhutekarla@gmail.com'
-    const obj: any = {};
-    if (Object.keys(obj).length === 0) {
-      Object.assign(obj, { "year": this.year, "email": this.email });
+    // this.email = localStorage.getItem('email')
+    this.objDetails = {};
+    if (Object.keys(this.objDetails).length === 0) {
+      Object.assign(this.objDetails, { "year": this.year, "email": this.email });
     }
-    this.apiService.candidateDashboard(obj).subscribe((res: any) => {
+    this.apiService.candidateDashboard(this.objDetails).subscribe((res: any) => {
       if (res.success) {
         this.candidateDahboard = res.data
         this.jobsAvailable = this.candidateDahboard.jobAvailableCount
         this.jobApplied = this.candidateDahboard.jobAppliedCount;
         this.profileView = this.candidateDahboard.profileViewedCount;
         this.shortlisted = this.candidateDahboard.shortlistedCount;
-        for (let i = 0; i < res.data.chatList.length; i++) {
-          const element = res.data.chatList[i]
-          // this.ChartjobAvailableCount=element.totalAvailableCount
-          // console.log('h',this.ChartjobAvailableCount)
-          this.chartOptions.series.push(element.totalAvailableCount);
-          this.chartOptions.series.push(element.jobAppliedCount);
-          this.chartOptions.series.push(element.profileViewedCount);
-          this.chartOptions.series.push(element.shortlistedCount);
-        }
+        this.ChartData = res.data.series;
+        this.chartOptions.series.push(this.ChartData)
       }
     })
   }
