f27f642489732a19f22ad9a41983e094cb730d21
murali <murali.b@lntecc.com>
2022-11-28 16:51:28 +0530

    applied jobs and saved job and candidate details bind

--- src/app/pages/candidate/job-display-pages/job-list-item/job-list-item.component.ts
+++ src/app/pages/candidate/job-display-pages/job-list-item/job-list-item.component.ts
@@ -1,4 +1,6 @@
 import {Component, Input, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/services/api.service';
+import { ToastrService } from 'ngx-toastr';
 @Component({
   selector: 'app-job-list-item',
   templateUrl: './job-list-item.component.html',
@@ -9,8 +11,41 @@ export class JobListItemComponent implements OnInit {
   public isDisabled:boolean = true;
   @Input() data:any;
   @Input() savedButton = false;
-  constructor() { }
+  constructor(private apiService: ApiService, private toastr: ToastrService) { }
   ngOnInit() {
   }
+  apply(item){
+   var obj ={};
+   obj={
+    "jobId":item.jobId,
+    "email":localStorage.getItem('email')
+   }
+  this.apiService.saveJobsDashboard(obj).subscribe((res:any)=>{
+    if (res.success) {
+      this.toastr.success('successfully applied')
+    }
+    else {
+      this.toastr.warning('Connection failed, Please try again.');
+    }
+  });
+  }
 
+  bookMarkIcon(item) {
+		item.isSelected = !item.isSelected;
+
+		let jobParams: any =
+		{
+			"email ": "deenabandhutekarla@gmail.com",
+			"jobId ": item.jobId
+		}
+		this.apiService.saveJobsDashboard(jobParams).subscribe((res: any) => {
+      if (res.success) {
+        this.toastr.success('successfully applied')
+      }
+      else {
+        this.toastr.warning('Connection failed, Please try again.');
+      }
+
+		});
+	}
 }
