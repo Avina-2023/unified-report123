fab3da08145d718b5671346f590ee1b1c4b07e39
murali <murali.b@lntecc.com>
2022-11-29 10:18:49 +0530

    sidebar added myprofile button and no saved jobs and no applied jobs center aligned

--- src/app/pages/candidate/candidate.component.ts
+++ src/app/pages/candidate/candidate.component.ts
@@ -2,8 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { MatDrawerMode } from '@angular/material/sidenav';
 import { NavigationEnd, Router, RouterEvent } from '@angular/router';
 import { ApiService } from 'src/app/services/api.service';
+import { AppConfigService } from 'src/app/utils/app-config.service';
 import { APP_CONSTANTS } from 'src/app/utils/app-constants.service';
-
+import { environment } from 'src/environments/environment';
 @Component({
   selector: 'app-candidatedash',
   templateUrl: './candidate.component.html',
@@ -15,7 +16,7 @@ export class CandidateComponent implements OnInit {
   isShowing: boolean = false;
   routelinks = APP_CONSTANTS.ENDPOINTS
   candidateName = localStorage.getItem('name')
-  constructor(public router:Router, private apiservice:ApiService) {
+  constructor(public router:Router, private apiservice:ApiService,private appConfig: AppConfigService) {
     this.router.events.subscribe(event => {
       if(event instanceof NavigationEnd) {
 
@@ -41,5 +42,10 @@ export class CandidateComponent implements OnInit {
   }
   logout(){
     this.apiservice.logout();
+  }
+  gotoProfile(){
+    let emailval = this.appConfig.getLocalStorage('email')
+    let enc_email = encodeURIComponent(this.apiservice.encryptnew(emailval,environment.cryptoEncryptionKey))
+    window.open(environment.SKILL_PROFILE_URL+'/externallogin?extId='+enc_email, 'profile_redir');
   }
 }
