3ab0e7dbc344abe19a177db402a34ebab36b0ee7
murali <murali.b@lntecc.com>
2022-12-16 12:18:58 +0530

    added jspdf package
    added jspdf package

--- src/app/pages/candidate/resume-builder/resume-builder.component.ts
+++ src/app/pages/candidate/resume-builder/resume-builder.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { Router } from '@angular/router';
 import { APP_CONSTANTS } from 'src/app/utils/app-constants.service';
-
+import { jsPDF } from "jspdf";
+import html2canvas from "html2canvas";
 
 @Component({
   selector: 'app-resume-builder',
@@ -27,5 +28,17 @@ export class ResumeBuilderComponent implements OnInit {
     this.router.navigateByUrl(APP_CONSTANTS.ENDPOINTS.CANDIDATEDASH.RESUMETEMPLATE);
     //this.router.navigateByUrl('/candidateview/findjobs');
   }
-  
+  generatePDF(){
+    console.log('hlo')
+    var data = document.getElementById('contentToConvert');
+    html2canvas(data).then(canvas=>{
+      let fileWidth = 208;
+      let fileHeight = (canvas.height * fileWidth) / canvas.width;
+      const FILEURI = canvas.toDataURL('image/png');
+      let PDF = new jsPDF('p', 'mm', 'a4');
+      let position = 0;
+      PDF.addImage(FILEURI, 'PNG', 0, position, fileWidth, fileHeight);
+      PDF.save('angular-demo.pdf');
+    })
+  }
 }
