bcd9598c9407f2f3a9225f39354b7ec57d47f47d
VENKATESH S <venkatesh.s1@lntecc.com>
2023-01-03 13:16:06 +0000

    Updated environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -19,6 +19,5 @@ export const environment = {
   SKILL_EDGE_URL :'https://skilledge.lntedutech.com',
   SKILL_PROFILE_URL :'https://profile.lntedutech.com',
   cryptoEncryptionKey:"(!@#Passcode!@#)",
-  SKILL_EDGE_URL: "https://skillexchange.lntedutech.com",
   blobToken:"?sv=2020-08-04&ss=bfqt&srt=sco&sp=rwdlacupitfx&se=2221-11-23T19:12:41Z&st=2021-11-23T11:12:41Z&spr=https,http&sig=R6%2BlZGrzjuFs1aAy2uUG%2BNkjVig5%2F8disv01i86VK8M%3D",
 };
