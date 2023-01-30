69be082c3dde89f2208dd740967f2158c6d2529b
Venkatesh <>
2023-01-30 17:54:39 +0530

    updated slider for employer
    updated slider for employer

--- src/app/pages/landing-screen/landing-employers/landing-employers.component.ts
+++ src/app/pages/landing-screen/landing-employers/landing-employers.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { APP_CONSTANTS } from 'src/app/utils/app-constants.service';
 import { OwlOptions } from 'ngx-owl-carousel-o';
+import { ApiService } from 'src/app/services/api.service';
+
 @Component({
   selector: 'app-landing-employers',
   templateUrl: './landing-employers.component.html',
@@ -8,8 +10,12 @@ import { OwlOptions } from 'ngx-owl-carousel-o';
 })
 export class LandingEmployersComponent implements OnInit {
   endPoints = APP_CONSTANTS.ENDPOINTS;
+  HiringPartners: any;
 
-  constructor() { }
+  constructor(private ApiService: ApiService) {
+    this.getFooterLogo();
+
+   }
 
   ngOnInit() {
   }
@@ -40,108 +46,12 @@ export class LandingEmployersComponent implements OnInit {
     nav: false,
   };
 
-  sliderdata1 = [
-    {
-      img1: 'https://assets.lntedutech.com/Skillexchange/partners/Lnt.png',
-    },
-    {
-      img1: 'https://assets.lntedutech.com/Skillexchange/partners/Lnt-Nxt.png',
-    },
-    {
-      img1: 'https://assets.lntedutech.com/Skillexchange/partners/Lnt-Tech.png',
-    },
-    {
-      img1: 'https://assets.lntedutech.com/Skillexchange/partners/LTI-lets-solve.png',
-    },
-    {
-      img1: 'https://assets.lntedutech.com/Skillexchange/partners/mindtree.png',
-    },
-    {
-      img1: 'https://assets.lntedutech.com/Skillexchange/partners/ABB_logo.png',
-    },
-    {
-      img1: 'https://assets.lntedutech.com/Skillexchange/partners/rane_logo.png',
-    },
-    {
-      img1: 'https://assets.lntedutech.com/Skillexchange/partners/Otis-Logo.png',
-    },
-    {
-      img1: 'https://assets.lntedutech.com/Skillexchange/partners/Aditya_Birla.png',
-    },
-    {
-      img1: 'https://assets.lntedutech.com/Skillexchange/partners/Inspirisys_logo.png',
-    },
-    {
-      img1: 'https://assets.lntedutech.com/Skillexchange/partners/Oppo.png',
-    },
-    {
-      img1: 'https://assets.lntedutech.com/Skillexchange/partners/MMC_Infotech.png',
-    },
-    {
-      img1: 'https://assets.lntedutech.com/Skillexchange/partners/Movate_logo.png',
-    },
-    {
-      img1: 'https://assets.lntedutech.com/Skillexchange/partners/prime.png',
-    },
-    {
-      img1: 'https://assets.lntedutech.com/Skillexchange/partners/Amadis-LOGO.jpg',
-    },
-    {
-      img1: 'https://assets.lntedutech.com/Skillexchange/partners/Corrohealth-logo.jpg',
-    },
-    {
-      img1: 'https://assets.lntedutech.com/Skillexchange/partners/Omega_logo.jpg',
-    },
-    {
-      img1: 'https://assets.lntedutech.com/Skillexchange/partners/Jouve_Logo.png',
-    },
-    {
-      img1: 'https://assets.lntedutech.com/Skillexchange/partners/Prime-Logo.jpg',
-    },
-    {
-      img1: 'https://assets.lntedutech.com/Skillexchange/partners/Vaken_logo.jpg',
-    },
-    {
-      img1: 'https://assets.lntedutech.com/Skillexchange/partners/Bankzone_logo.jpg',
-    },
-    {
-      img1: 'https://assets.lntedutech.com/Skillexchange/partners/grootan.png',
-    },
-    {
-      img1: 'https://assets.lntedutech.com/Skillexchange/partners/rudram.png',
-    },
-    {
-      img1: 'https://assets.lntedutech.com/Skillexchange/partners/Bankbazaar-Logo.png',
-    },
-    {
-      img1: 'https://assets.lntedutech.com/Skillexchange/partners/BeumerGroup_Logo.webp',
-    },
-    {
-      img1: 'https://assets.lntedutech.com/Skillexchange/partners/AhamHousingFinance_logo.webp',
-    },
-    {
-      img1: 'https://assets.lntedutech.com/Skillexchange/partners/Amaindia-logo.jpg',
-    },
-    {
-      img1: 'https://assets.lntedutech.com/Skillexchange/partners/Ideas2IT_logo.webp',
-    },
-    {
-      img1: 'https://assets.lntedutech.com/Skillexchange/partners/techberg_logo.webp',
-    },
-    {
-      img1: 'https://assets.lntedutech.com/Skillexchange/partners/bet_logo.webp',
-    },
-    {
-      img1: 'https://assets.lntedutech.com/Skillexchange/partners/codebios_logo.webp',
-    },
-    {
-      img1: 'https://assets.lntedutech.com/Skillexchange/partners/altimetrik_logo.webp',
-    },
-    {
-      img1: 'https://assets.lntedutech.com/Skillexchange/partners/maruti_logo.webp',
-    },
-    {
-      img1: 'https://assets.lntedutech.com/Skillexchange/partners/sba_logo.webp',
-    },
-  ];
+
+  getFooterLogo(){
+    this.ApiService.partnerfooterlist({}).subscribe((response: any) => {
+        if(response.success){
+           this.HiringPartners = response.data.HiringPartners;
+        }
+    })
+  }
 }
