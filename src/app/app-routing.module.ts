f8feef17b63326d7d7638f1a71045d6e713c8446
Maskooth Farook <88648646+farookdarkcode@users.noreply.github.com>
2023-04-13 16:11:48 +0530

    commit with module added in app-module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -42,6 +42,9 @@ const routes: Routes = [
     path: `certificate`, component: CertificateViewComponent
   },
   {
+    path: `${APP_CONSTANTS.ROUTES.REPORTS.HOME}`, loadChildren: () => import('./pages/view-reports/view-reports.module').then(m => m.ViewReportsModule), data: { key: 'user' }
+  },
+  {
     path: `error`,
     pathMatch: 'full',
     component: PagenotfoundComponent
