ec46fb9c28f2ae8df3033b3785eb47211aacb42f
santhosh <santhoshrajmanogar@gmail.com>
2023-08-08 14:58:51 +0530

    bugs in employer and dashboard

--- src/app/pages/candidate/job-display-pages/job-list-item/job-list-item.component.ts
+++ src/app/pages/candidate/job-display-pages/job-list-item/job-list-item.component.ts
@@ -9,36 +9,39 @@ import { environment } from 'src/environments/environment';
   templateUrl: './job-list-item.component.html',
   styleUrls: ['./job-list-item.component.scss'],
 })
-export class JobListItemComponent implements OnInit ,AfterViewInit {
+export class JobListItemComponent implements OnInit, AfterViewInit {
   public isActive: boolean = true;
   public isDisabled: boolean = true;
-  public currentdate:any = new Date();
+  public currentdate: any = new Date();
   @Input() data: any;
   @Input() savedButton = false;
   @Input() showApplied = false;
-  blobToken = environment.blobToken
-	productionUrl = environment.SKILL_EDGE_URL == "https://skilledge.lntedutech.com"?true:false;
- 
+  blobToken = environment.blobToken;
+  productionUrl =
+    environment.SKILL_EDGE_URL == 'https://skilledge.lntedutech.com'
+      ? true
+      : false;
+
   constructor(
     private apiService: ApiService,
     private toastr: ToastrService,
-    public router:Router
+    public router: Router
   ) {}
   ngOnInit() {
-    this.currentdate =this.currentdate.toISOString()
+    this.currentdate = this.currentdate.toISOString();
   }
   apply(item) {
     var obj = {};
     obj = {
       jobId: item.jobId,
       email: localStorage.getItem('email'),
-      companyId:item.companyId,
-      jobDetails:{
-        education:item.education,
-        specialization:item.specialization,
-        yearofPassout:item.yearofPassout,
-        eligibilityCriteria:item.eligibilityCriteria
-      }
+      companyId: item.companyId,
+      jobDetails: {
+        education: item.education,
+        specialization: item.specialization,
+        yearofPassout: item.yearofPassout,
+        eligibilityCriteria: item.eligibilityCriteria,
+      },
     };
     this.apiService.savedJobs(obj).subscribe((res: any) => {
       if (res.success) {
@@ -60,16 +63,16 @@ export class JobListItemComponent implements OnInit ,AfterViewInit {
       if (res.success) {
         this.toastr.success(res.message);
         let currentUrl = this.router.url;
-        this.router.navigateByUrl('/', {skipLocationChange: true}).then(() => {
+        this.router
+          .navigateByUrl('/', { skipLocationChange: true })
+          .then(() => {
             this.router.navigate([currentUrl]);
-        });
+          });
       } else {
         this.toastr.warning(res.message);
       }
     });
   }
 
-  ngAfterViewInit() {
-
-  }
+  ngAfterViewInit() {}
 }
