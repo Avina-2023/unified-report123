a35536503cc7a2d1d6f3ad3fab1c44db4af3297c
avinash pasupuleti <pasupuleti.avinash@lntecc.com>
2023-09-14 11:26:48 +0530

    new component for overall report
    new component for overall report

---
+++ src/app/pages/employer/overall-Report/overall-Report.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-overall-Report',
+  templateUrl: './overall-Report.component.html',
+  styleUrls: ['./overall-Report.component.scss']
+})
+export class OverallReportComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
