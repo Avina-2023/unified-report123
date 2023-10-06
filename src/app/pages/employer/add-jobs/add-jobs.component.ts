c6ec2999af679c1e2eb8611d95d2cbc57b010678
santhosh <santhoshrajmanogar@gmail.com>
2023-10-06 17:22:39 +0530

    ctc radio button and regex validation for url and date

--- src/app/pages/employer/add-jobs/add-jobs.component.ts
+++ src/app/pages/employer/add-jobs/add-jobs.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-jobs',
@@ -14,51 +14,78 @@ export class AddJobsComponent implements OnInit {
     'Option 3',
     // Add more options as needed
   ];
+  ctcArray = ['Option 1', 'Option 2', 'Option 3'];
+  rangefromArray = ['Option A', 'Option B', 'Option C'];
+  rangetoArray = ['Option X', 'Option Y', 'Option Z'];
+  // selectedOption: string = 'jobs';
+   selectedOption: string = '1';
+
+
+
+
   constructor(private fb: FormBuilder) {
-    this.addjobsForm = this.fb.group({
-      industryType: [''], // Initialize the form control for the select element
-    });
+    // this.addjobsForm = this.fb.group({
+    //   fixedctc: [''],
+    //   startrangectc: [''],
+    //   endrangectc: [''],
+    //   ctcOptions: ['1'],
+    // });
   }
 
   ngOnInit(): void {
     // this.getRoute();
     this.formerrorInitialize();
     // this.getIndustryType();
+
   }
   formerrorInitialize() {
-    const emailregex: RegExp =
-      /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    // const emailregex: RegExp =
+    //   /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
     this.addjobsForm = this.fb.group({
+
+ fixedctc: [''],
+      startrangectc: [''],
+      endrangectc: [''],
+      ctcOptions: ['1'],
+
       CompanyName: ['', [Validators.required]],
       JobRole: ['', [Validators.required]],
-      //   [
-      //   '',
-      //   Validators.compose([
-      //     Validators.required,
-      // Validators.minLength(4),
-      // Validators.maxLength(4),
-      // Validators.pattern('[1-9]{1}[0-9]{3}'),
-      //   ]),
-      // ],
       JobLocation: ['', [Validators.required]],
       JobType: ['', [Validators.required]],
       degree: ['', [Validators.required]],
       specialization: ['', [Validators.required]],
       keyskill: ['', [Validators.required]],
       lastdate: ['', [Validators.required]],
-      url: ['', [Validators.required, this.urlValidator()]],
-      mobile: [
+      url: [
         '',
-        Validators.compose([
+        [
           Validators.required,
-          Validators.minLength(10),
-          Validators.maxLength(10),
-          Validators.pattern('[1-9]{1}[0-9]{9}'),
-        ]),
+          Validators.pattern(/^(https?:\/\/)?([\w\d.-]+)\.([a-z]{2,})(\/\S*)?$/i)
+        ]
       ],
+       yearPassing: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern(/^[0-9]+$/),
+          Validators.minLength(4),
+          Validators.maxLength(4),
+        ]
+      ],
+      // mobile: [
+      //   '',
+      //   Validators.compose([
+      //     Validators.required,
+      //     Validators.minLength(10),
+      //     Validators.maxLength(10),
+      //     Validators.pattern('[1-9]{1}[0-9]{9}'),
+      //   ]),
+      // ],
       description: ['', [Validators.required]],
       requirements: ['', [Validators.required]],
+            // ctcOptions: ['1'],
+
     });
   }
   get CompanyName() {
@@ -98,21 +125,8 @@ export class AddJobsComponent implements OnInit {
   get requirements() {
     return this.addjobsForm.get('requirements');
   }
-
-  urlValidator(): Validators {
-    return (control) => {
-      if (control.value) {
-        // Regular expression to validate URLs
-        const urlPattern = /^(https?:\/\/)?([\w\d.-]+)\.([a-z]{2,})(\/\S*)?$/i;
-
-        if (!urlPattern.test(control.value)) {
-          // Return a validation error if URL is invalid
-          return { invalidUrl: true };
-        }
-      }
-
-      // Return null if the URL is valid or empty
-      return null;
-    }
+  get yearPassing() {
+    return this.addjobsForm.get('yearPassing');
   }
+
 }
