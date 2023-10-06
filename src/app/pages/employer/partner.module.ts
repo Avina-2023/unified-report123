92fbc7f20c3f7cc9bc3f05e19f5a4a5ca3e302bc
venkat.j1 <venkat.j1@lntecc.com>
2023-10-06 17:56:34 +0530

    Post Job Requirements Page
    Post Job Requirements Page

--- src/app/pages/employer/partner.module.ts
+++ src/app/pages/employer/partner.module.ts
@@ -19,7 +19,9 @@ import { NzSelectModule } from 'ng-zorro-antd/select';
 import { OverallReportComponent } from './overall-Report/overall-Report.component';
 import { OverallReportDetailsCardComponent } from './overall-Report/overall-Report-details-card/overall-Report-details-card.component';
 import { OverallReportMinicardDetailsComponent } from './overall-Report/overall-Report-minicard-details/overall-Report-minicard-details.component';
-// import { AngularEditorModule } from '@kolkov/angular-editor';
+import { AngularEditorModule } from '@kolkov/angular-editor';
+
+
 
 @NgModule({
   declarations: [
@@ -34,7 +36,7 @@ import { OverallReportMinicardDetailsComponent } from './overall-Report/overall-
     OverallReportComponent,
     OverallReportDetailsCardComponent,
     OverallReportMinicardDetailsComponent
-
+    
   ],
   imports: [
     CommonModule,
@@ -46,7 +48,7 @@ import { OverallReportMinicardDetailsComponent } from './overall-Report/overall-
     ReactiveFormsModule,
     NzDropDownModule,
     NzSelectModule,
-    // AngularEditorModule
+    AngularEditorModule
   ],
 })
 export class Adminmodule { }
