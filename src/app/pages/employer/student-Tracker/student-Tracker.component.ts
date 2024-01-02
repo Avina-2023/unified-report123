54e1fc6eaa3fc042848302f93a195cbf2941fa10
avinash pasupuleti <pasupuleti.avinash@lntecc.com>
2024-01-02 10:33:10 +0530

    Student tracker Api binding for charts
    Student tracker Api binding for charts

--- src/app/pages/employer/student-Tracker/student-Tracker.component.ts
+++ src/app/pages/employer/student-Tracker/student-Tracker.component.ts
@@ -6,6 +6,7 @@ import { ColDef, GridApi } from 'ag-grid-community';
 import { AppConfigService } from 'src/app/utils/app-config.service';
 import { APP_CONSTANTS } from '../../../utils/app-constants.service';
 import { GridOptions } from '@ag-grid-enterprise/all-modules';
+import { AgGridAngular } from 'ag-grid-angular';
 import { ChartType, ChartOptions } from 'chart.js';
 import { MultiDataSet, Label, Colors } from 'ng2-charts';
 import { Router } from '@angular/router';
@@ -26,8 +27,27 @@ export class StudentTrackerComponent implements OnInit {
   public serverSideStoreType;
   overallreportdata: any;
 
+
+  selectedYear: number;
+  selectedMonth: number;
+  years: number[] = [];
+  months = [
+    { name: 'January', value: 1 },
+    { name: 'February', value: 2 },
+    { name: 'March', value: 3 },
+    { name: 'April', value: 4 },
+    { name: 'May', value: 5 },
+    { name: 'June', value: 6 },
+    { name: 'July', value: 7 },
+    { name: 'August', value: 8 },
+    { name: 'September', value: 9 },
+    { name: 'October', value: 10 },
+    { name: 'November', value: 11 },
+    { name: 'December', value: 12 },
+  ];
+
   public overlayNoRowsTemplate =
-  ' <span><br><br><img src="assets/images/skillMaster/norecord.svg" alt="" /> <br><br> <h3>No Records Found</h3></span>';
+    ' <span><br><br><img src="assets/images/skillMaster/norecord.svg" alt="" /> <br><br> <h3>No Records Found</h3></span>';
 
   colorconfig = [
     {
@@ -74,85 +94,237 @@ export class StudentTrackerComponent implements OnInit {
   public doughnutChartColors: Colors[] = [{
     backgroundColor:
       [
-        '#2862FF',
-        '#5289C9',
-        '#EB4A8A',
-        '#FF6A81',
+        '#FFB74D',
+        '#56B35A',
+        '#EF2917',
       ],
-
   }
   ];
 
-  doughnutChartLabels: Label[] = ['Skill Profiles Filled', 'Open Job Views', 'Internship Applications', 'Job Applications'];
+  doughnutChartLabels: Label[] = ['Trained', 'Assessed', 'Others'];
   doughnutChartData: MultiDataSet = [];
   doughnutChartType: ChartType = 'doughnut';
 
+  public options2: ChartOptions = {
+    responsive: true,
+    layout: {
+      padding: {
+        left: 0,
+        right: 0,
+        bottom: 0,
+        top: 0
+      }
+    },
+    legend: {
+      display: false,
+    },
+    plugins: {
+      datalabels: {
+        display: false
+      }
+    },
+    cutoutPercentage: 70
+  }
+
+  public doughnutChartColors2: Colors[] = [{
+    backgroundColor:
+      [
+        '#FFB74D',
+        '#56B35A',
+        '#EF2917',
+      ],
+  }
+  ];
+
+  doughnutChartLabels2: Label[] = ['Trained', 'Assessed', 'Others'];
+  doughnutChartData2: MultiDataSet = [];
+  doughnutChartType2: ChartType = 'doughnut';
+
+  studentData: any;
+  public themeClass: string = "ag-theme-quartz";
+  studentChartData: any;
   constructor(
-    private router:Router,
+    private router: Router,
     private apiService: ApiService,
     private appConfig: AppConfigService
-  ) { } 
+  ) { }
 
   ngOnInit() {
-    this.getOverAllReport();
-   }
+    this.getCurrentMonth();
+    //this.getagGridData();
+    this.generateYearOptions();
+  }
 
+  //  getCurrentMonth(){
+  //   const currentYear = '2023';//this.currentyear using js
+  //     const currentMonth = '10'
+  //     let obj = {
+  //       "year": currentYear,
+  //       "month": currentMonth
+  //   }
+  //   this.getagGridData(obj);
+  //  }
 
-  rowData = [
-    { make: 'Toyota', model: 'Celica', price: 35000 },
-    { make: 'Ford', model: 'Mondeo', price: 32000 },
-    { make: 'Porsche', model: 'Boxster', price: 72000 }
-  ];
+  //  changeMonthYear(){
+  //   const currentYear = '2023'; // get the current changed value in select dropdown
+  //     const currentMonth = '10';
+  //   let changedDate = {
+  //     "year": currentYear,
+  //     "month": currentMonth 
+  //   }
+  //   this.getagGridData(changedDate);
+  //  }
+
+  onGridReady(params) {
+    this.gridApi = params.api;
+  }
+
+  getCurrentMonth() {
+    const currentDate = new Date();
+    this.selectedYear = currentDate.getFullYear();
+    this.selectedMonth = currentDate.getMonth() + 1; // Months are 0-based
+    this.getagGridData({ year: this.selectedYear, month: this.selectedMonth });
+  }
+
+  generateYearOptions() {
+    const currentYear = new Date().getFullYear();
+    for (let year = 2019; year <= currentYear; year++) {
+      this.years.push(year);
+    }
+  }
+
+  changeMonthYear() {
+    // this.getagGridData({ year: this.selectedYear, month: this.selectedMonth });
+    // console.log(this.selectedYear, this.selectedMonth, 'Changing month and year:');
+
+    const year = +this.selectedYear;
+    const month = +this.selectedMonth;
+    this.getagGridData({ year, month });
+    console.log(year, month, 'Changing month and year:');
+  }
+
+  rowData = [];
 
   columnDefs = [
-    { headerName: 'Parent 1', field: 'make' },
+    { headerName: 'Date', resizable: true, field: 'date', cellStyle: { 'border-right-color': '#e2e2e2' }, },
     {
-      headerName: 'Parent 2',
+      headerName: 'Registrations', resizable: true, cellStyle: { 'border-right-color': '#e2e2e2' },
       children: [
-        { headerName: 'Child 1', field: 'model' },
-        { headerName: 'Child 2', field: 'price' },
-        { headerName: 'Child 3', field: 'make' }
+        { headerName: 'Day', resizable: true, cellStyle: { 'border-right-color': '#e2e2e2' }, field: 'regDayCount', },
+        { headerName: 'YTD', resizable: true, cellStyle: { 'border-right-color': '#e2e2e2' }, field: 'regYtdCount' },
       ]
     },
     {
-      headerName: 'Parent 3',
+      headerName: 'Skill Profile', resizable: true,
       children: [
-        { headerName: 'Child 1', field: 'price' },
+        { headerName: 'Day', resizable: true, field: 'profDayCount', cellStyle: { 'border-right-color': '#e2e2e2' }, },
+        { headerName: 'YTD', resizable: true, field: 'profYtdCount', cellStyle: { 'border-right-color': '#e2e2e2' }, },
+      ]
+    },
+    {
+      headerName: 'L&T EduTech Trained', resizable: true,
+      children: [
         {
-          headerName: 'Child 2',
+          headerName: 'Registrations', resizable: true,
           children: [
-            { headerName: 'Grandchild 1', field: 'make' },
-            { headerName: 'Grandchild 2', field: 'model' }
+            { headerName: 'Day', resizable: true, field: 'trainRegDayCount', cellStyle: { 'border-right-color': '#e2e2e2' }, },
+            { headerName: 'YTD', resizable: true, field: 'trainRegYtdCount', cellStyle: { 'border-right-color': '#e2e2e2' }, },
+
           ]
-        }
+        },
+        {
+          headerName: 'Profile Filled', resizable: true,
+          children: [
+            { headerName: 'Day', resizable: true, field: 'trainProfDayCount', cellStyle: { 'border-right-color': '#e2e2e2' }, },
+            { headerName: 'YTD', resizable: true, field: 'trainProfYtdCount', cellStyle: { 'border-right-color': '#e2e2e2' }, },
+
+          ]
+        },
       ]
     },
-    { headerName: 'Parent 4', field: 'model' },
-   
+    {
+      headerName: 'L&T EduTech Assessed', resizable: true,
+      children: [
+        {
+          headerName: 'Registrations', resizable: true,
+          children: [
+            { headerName: 'Day', resizable: true, field: 'assessRegDayCount', cellStyle: { 'border-right-color': '#e2e2e2' }, },
+            { headerName: 'YTD', resizable: true, field: 'assessRegYtdCount', cellStyle: { 'border-right-color': '#e2e2e2' }, },
+
+          ]
+        },
+        {
+          headerName: 'Profile Filled', resizable: true,
+          children: [
+            { headerName: 'Day', resizable: true, field: 'assessProfDayCount', cellStyle: { 'border-right-color': '#e2e2e2' }, },
+            { headerName: 'YTD', resizable: true, field: 'assessProfYtdCount', cellStyle: { 'border-right-color': '#e2e2e2' }, },
+
+          ]
+        },
+      ]
+    }
+
   ];
 
+  // getagGridData(obj) {
+  //   this.apiService.getStudentTrackerReport(obj).subscribe((response: any) => {
+  //     if (response.success) {
+  //       this.studentData = response.data
+  //       console.log(this.studentData, 'overalldata');
+  //       this.rowData = response.data.reportData
+  //       // console.log(this.studentData.reportData,'studentDatastudentData');
+  //       // this.studentChartData = response.data.chartData
+  //       // console.log(this.studentChartData,'studentDatastudentData');
+  //       let chartData = [
+  //         this.studentData.chartData.registerTrainTotal,
+  //         this.studentData.chartData.registerAssessTotal,
+  //         this.studentData.chartData.otherRegisterTotal
+  //       ]
+  //       this.doughnutChartData.push(chartData);
 
-  getagGridData(){
-  }
+  //       let chartData2 = [
+  //         this.studentData.chartData.profileTrainTotal,
+  //         this.studentData.chartData.profileAssessTotal,
+  //         this.studentData.chartData.otherProfileTotal
+  //       ]
+  //       this.doughnutChartData2.push(chartData2);
+  //     }
+  //   })
+  // }
 
-  exportData(){
-    this.router.navigate(['/auth/overall-reports'])
-  }
-
-  getOverAllReport() {
-    this.apiService.getOverAllReport(data).subscribe((response: any) => {
-      if (response.success) {
-        this.overallreportdata = response.data
-        let chartData = [
-          this.overallreportdata.eduTechReport.skillProfileCount,
-          this.overallreportdata.eduTechReport.openJobViewCount,
-          this.overallreportdata.eduTechReport.internshipApplicationCount,
-          this.overallreportdata.eduTechReport.jobApplicationCount
-        ]
-        this.doughnutChartData.push(chartData);
-        console.log(this.overallreportdata, 'overallreportdata');
+  getagGridData(obj) {
+    this.rowData = [];
+    this.apiService.getStudentTrackerReport(obj).subscribe(
+      (response: any) => {
+        if (response.success) {
+          this.studentData = response.data;
+          console.log(this.studentData, 'overalldata');
+          this.rowData = response.data.reportData;
+          this.doughnutChartData = [];
+          let chartData = [
+            this.studentData.chartData.registerTrainTotal,
+            this.studentData.chartData.registerAssessTotal,
+            this.studentData.chartData.otherRegisterTotal
+          ]
+          this.doughnutChartData.push(chartData);
+          this.doughnutChartData2 = [];
+          let chartData2 = [
+            this.studentData.chartData.profileTrainTotal,
+            this.studentData.chartData.profileAssessTotal,
+            this.studentData.chartData.otherProfileTotal
+          ]
+          this.doughnutChartData2.push(chartData2);
+        }
+      },
+      (error) => {
+        console.error('API Error:', error);
+        this.rowData = [];
       }
-    })
+    );
+  }
+
+  exportData() {
+    this.gridApi.exportDataAsExcel();
   }
 
 }
